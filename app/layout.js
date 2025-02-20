d92c1e3e9d8cc99bdc4c21a68a8f708ea93ed1e5
Alpinden <denisalpin3@gmail.com>
2025-02-20 18:19:58 +0300

    Added layout.js for Next.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
